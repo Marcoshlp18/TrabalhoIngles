54f8276a907326365623886373dcd528f33ff8df
Marcoshlp18 <marcoshenriqueball12@gmail.com>
2022-06-30 15:46:43 -0300

    audaces fortuna juvat

---
+++ script.js
@@ -0,0 +1,60 @@
+
+let Comedy = [
+  { title:"If You Drink Don't Marry",
+    director:"Todd Phillips",
+    year:2009
+  },
+
+  { title:"A Leprechaun in New York",
+    director:"Jon Favreau",
+    year:2003
+  },
+
+  { title:"My mother is a piece",
+    director:"André Pellenz",
+    year:2013
+
+  },
+
+]
+
+console.log(Comedy);
+
+let Action = [
+  { title:"Morbidus",
+    director:"Daniel Espinosa",
+    year:2022
+  },
+
+  { title:"Matrix",
+    director:"Lana Wachowski,Lilly Wachowski",
+    year:1999
+  },
+
+  { title:"The Toronto Man",
+    director:"Patrick Hughes",
+    year:2022
+  },
+
+]
+
+console.log(Action);
+
+let Fantasy = [
+  { title:"Doctor Strange in the Multiverse of Madness",
+    director:"Sam Raimi",
+    year:2022
+  },
+
+  { title:"Interstellar",
+    director:"Christopher Nolan",
+    year:2014
+  },
+
+  { title:"Lucy",
+    director:"Luc Besson",
+    year:2014
+  },
+
+]
+console.log(Fantasy);
